2faa3b3cc3005a7f800cc3dbc500fe846989a103
davidmchale <dmchale@squiz.net>
2023-11-23 13:05:54 +1100

    stars component for movie app

---
+++ src/global/stars/StarRating.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+import Stars from './Stars';
+
+const StarRating = ({maxLength, defaultRating = 0}) => {
+
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0)
+
+  // styles
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };
+  
+  const starContainerStyle = {
+    display: "flex",
+  };
+
+
+
+  const handleRating = (rating) => {
+    setRating(rating)
+  }
+
+
+  
+  return (
+    <div style={containerStyle}>
+    <div style={starContainerStyle}>
+
+     
+    
+    {Array.from({ length: maxLength }, (_, i) => (
+          <Stars
+            key={i}
+            size={30}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            />
+        ))}
+
+   
+     
+    </div>
+    </div>
+  )
+}
+
+export default StarRating
+
+
+StarRating.defaultProps = {
+  maxLength: 10,
+}
